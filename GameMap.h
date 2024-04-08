dd202dfde464c0e873ea6594556187f64be3fa65
Vy <23020717@vnu.edu.vn>
2024-04-08 15:25:24 +0700

    GameMap.h

---
+++ GameMap.h
@@ -0,0 +1,30 @@
+#ifndef GAME_MAP_H
+#define GAME_MAP_H
+#include "CommonFunc.h"
+#include"BaseObject.h"
+#include <cstdio>
+
+#define MAX_TILES 20
+
+class TileMap : public BaseObject
+{
+public:
+    TileMap(){;}
+    ~TileMap(){;}
+};
+class GameMap
+{
+public:
+    GameMap(){;}
+    ~GameMap(){;}
+
+    void LoadMap(  char* name);
+    void LoadTiles(SDL_Renderer* screen);
+    void DrawMap(SDL_Renderer* screen);
+private:
+  map game_map_;//chứa toàn bộ thông tin: trạng thái của ô , vị trí của ô,chỉ sô của ô
+  TileMap tile_map_[MAX_TILES];
+
+
+};
+#endif // GAME_MAP_H
