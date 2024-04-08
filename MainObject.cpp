6b4b9a2b56df5e4e70f81b9ec1acd40730c2da89
Vy <23020717@vnu.edu.vn>
2024-04-08 22:06:28 +0700

    1

--- MainObject.cpp
+++ MainObject.cpp
@@ -10,6 +10,12 @@ MainObject::MainObject ()
     width_frame_=0;
     height_frame_=0;
     status_=-1;
+    input_type_.right_=0;
+    /*input_type_.left_=0;
+    input_type_.down_=0;
+    input_type_.up_=0;
+    input_type_.jump_=0;*/
+    on_ground_=false;
 
 }
 MainObject::~MainObject ()
@@ -109,7 +115,6 @@ void MainObject ::HandelInputAction(SDL_Event events, SDL_Renderer* screen)
         case SDLK_RIGHT :
             {  status_=WALK_RIGHT;
               input_type_.right_=1;
-
             }
             break;
 
@@ -129,3 +134,111 @@ void MainObject ::HandelInputAction(SDL_Event events, SDL_Renderer* screen)
 
     }
 }
+void MainObject::DoPlayer(map& map_data)
+{
+    x_val_=0;
+    y_val_+=1; //0.8:GRAVITY_SPEED
+
+    if(y_val_>=MAX_FALL_SPEED)
+    {
+        y_val_= MAX_FALL_SPEED;
+    }
+
+    if(input_type_.right_==1)
+    {
+        x_val_+=PLAYER_SPEED;
+    }
+
+    CheckToMap(map_data);
+}
+void MainObject::CheckToMap(map& map_data)
+{
+   int x1=0;
+   int x2=0;
+
+   int y1=0;
+   int y2=0;
+
+   int height_min = height_frame_< TILE_SIZE ? height_frame_:TILE_SIZE;
+
+   x1= (x_pos_ + x_val_)/TILE_SIZE;
+   x2=(x_pos_+x_val_+width_frame_ -1 )/TILE_SIZE;
+
+   y1=(y_pos_)/TILE_SIZE;
+   y2=(y_pos_+height_min-1)/TILE_SIZE;
+
+   if(x1>=0&& x2<=MAX_MAP_X&&y1>=0&&y2<MAX_MAP_Y)
+   {
+       if(x_val_>0)// moving to right
+       {
+           if(map_data.tile[y1][x2]!=BLANK_TILE|| map_data.tile[y2][x2]!=BLANK_TILE)
+           {
+               x_pos_ =x2*TILE_SIZE;
+               x_pos_-=(width_frame_+1);
+               x_val_=0;
+           }
+
+
+       }
+   }
+//check vertical
+
+int width_min= width_frame_< TILE_SIZE ? width_frame_: TILE_SIZE;
+
+  x1=(x_pos_)/TILE_SIZE;
+  x2=(x_pos_+width_min)/TILE_SIZE;
+
+  y1=(y_pos_+y_val_)/TILE_SIZE;
+  y2=(y_pos_+y_val_+height_frame_-1)/TILE_SIZE;
+
+  if(x1>=0&&x2< MAX_MAP_X && y1>=0&&y2< MAX_MAP_Y)
+  {
+      if(y_val_>0)//roi tu do
+      {
+          if(map_data.tile[y2][x1]!=BLANK_TILE|| map_data.tile[y2][x2]!=BLANK_TILE)
+          {
+              y_pos_=y2*TILE_SIZE;
+              y_pos_ -=(height_frame_+1);
+              y_val_=0;
+              on_ground_=true;
+
+          }
+      }
+    else if(y_val_<0)
+    {
+            if(map_data.tile[y1][x1]!=BLANK_TILE|| map_data.tile[y1][x2]!=BLANK_TILE)
+            {
+                y_pos_=(y1+1)*TILE_SIZE;
+                y_val_=0;
+            }
+
+    }
+  }
+  x_pos_+=x_val_;
+  y_pos_+=y_val_;
+
+if(x_pos_+width_frame_>map_data.max_x_)
+{
+    x_pos_=map_data.max_x_-width_frame_-1;// tru sai so
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
