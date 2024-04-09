9aa83ac7f608e83c843e1c8d7c75318d759e4dc5
Vy <23020717@vnu.edu.vn>
2024-04-09 10:44:13 +0700

    2

--- GameMap.h
+++ GameMap.h
@@ -21,7 +21,8 @@ public:
     void LoadMap(  char* name);
     void LoadTiles(SDL_Renderer* screen);
     void DrawMap(SDL_Renderer* screen);
-    map getMap()const {return game_map_;};
+    map getMap()const {return game_map_;};//phan di chuyen nhan vat
+    void SetMap(map& map_data){ game_map_=map_data;};// de map chay theo nhan vat
 private:
   map game_map_;//chứa toàn bộ thông tin: trạng thái của ô , vị trí của ô,chỉ sô của ô
   TileMap tile_map_[MAX_TILES];
